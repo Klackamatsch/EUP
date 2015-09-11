97cc65128b778830f4851239057e103cb5c43361
Klackamatsch <sarah.kaehlig@gmx.de>
2015-09-11 09:06:20 +0200

    was passiert ist

---
+++ ruby/Code/flicks.rb
@@ -0,0 +1,50 @@
+def movie_listing(title)
+
+"Movie: #{title.downcase.capitalize}"
+
+end
+
+puts movie_listing("Ninja Turtles")
+puts movie_listing("SWAT")
+puts movie_listing("American Horror Story")
+
+
+
+
+
+
+
+
+#Beispiel für Variablen innerhalb von methoden
+#def movie_listing
+# 		var1 = "Movie: American Horror Story"
+# 		var2 =  "Movie: Bee Movie"
+#puts	"#{var1}\n#{ var2}"
+# 		"Movie: checkie chan"
+# 		"Movie: rambo"
+# end 
+
+# puts movie_listing
+
+# var = movie_listing
+# puts "Rückgabewert #{var}"
+# puts "var außerhalb:#{var.object_id}"
+
+
+# def music_listening
+
+# 			"trck1: magic carpet ride"
+# 			"trck2: hotel california"
+# 	end
+
+# 	puts music_listening
+
+# def lalala
+
+# puts"erste zeile"
+# puts "zweite zeile"
+
+# end
+#  lalala
+
+
