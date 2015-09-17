3b8d4fb7aa551082370a833e118d739ed362e949
Klackamatsch <sarah.kaehlig@gmx.de>
2015-09-17 10:52:15 +0200

    hallo#

--- ruby/Code/flicks.rb
+++ ruby/Code/flicks.rb
@@ -1,12 +1,73 @@
-def movie_listing(title)
+class Movie
+	
 
-"Movie: #{title.downcase.capitalize}"
+	def initialize(ptitle,prank=0)
+			puts"neues Objekt"
+			@title = ptitle.capitalize
+			@rank = prank
+		end
+
+	def thumbs_up
+		@rank += 1
+		#@rank = @rank +1
+		end
+
+	def Thumbs_down
+			@rank -= 1
+			#@rank = @rank - 1
+
+		end
+
+
+	def to_s
+			"#{@title} hat ein Ranking von: #{@rank}."
+	
+		end
+		# def title
+		# 	@title
+			
+		# end
+attr_reader :title   # attr_reader macht das selbe wie die methode über dem hier
+
+#def title=(ptitle)
+	#@title = ptitle
+	
+#end
+
+attr_writer :title # attr_writer ersetzt die oberen 3 zeilen
 
 end
 
-puts movie_listing("Ninja Turtles")
-puts movie_listing("SWAT")
-puts movie_listing("American Horror Story")
+movie1 = Movie.new('Ninja Turtles',4)
+puts movie1.to_s
+movie1.thumbs_up
+puts movie1.to_s
+movie1.Thumbs_down
+puts movie1.to_s
+
+puts movie1.title
+
+movie1.title= "ninja purples"
+puts movie1
+
+
+
+
+
+
+
+# def movie_listing(title,rank=10)
+
+# "Movie: #{title.downcase.capitalize} hat #{weekday} das Ranking #{rank}"
+
+# end
+# def weekday
+#  Time.now.strftime("%A")
+# end
+
+# puts movie_listing("Ninja Turtles",1)
+# puts movie_listing("SWAT",5)
+# puts movie_listing("American Horror Story",3)
 
 
 
