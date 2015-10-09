237ca1de436e3db778a3b306b263faef44ca354c
Klackamatsch <sarah.kaehlig@gmx.de>
2015-10-09 09:27:00 +0200

    freitag

---
+++ ruby/aufgabe10.rb/anweisung.rb
@@ -0,0 +1,33 @@
+require_relative 'klassekunde'
+require_relative 'klassekredit'
+
+kunde1= Kunde.new('theo sommer')
+puts "STEP 1 **********************************************************\n"
+puts kunde1.huhu
+
+kunde1.adresse = ('Hermelinweg11, 24567 Hamburg')
+puts "STEP 2 **********************************************************\n"
+puts kunde1.huhu
+
+kunde1.name = ('Theo Sonnenschein')
+puts "STEP 3 **********************************************************\n"
+puts kunde1.huhu
+
+kunde1.gehalt = 2000
+puts "STEP 4 **********************************************************\n"
+puts kunde1.huhu
+
+
+
+
+kredit1 = Kredit.new(kunde1.huhu)
+
+
+puts "STEP 5 *******************************************************\n"
+puts kredit1
+
+kredit1.jahresgehalt = 24000
+puts "STEP 6 ********************************************************\n"
+puts kredit1.gehalt
+
+
--- ruby/aufgabe10.rb/klassekunde.rb
+++ ruby/aufgabe10.rb/klassekunde.rb
@@ -1,15 +1,21 @@
 
 # 1.  Klasse Kunde
-# 1a. Legen Sie eine Eigenschaft name und adresse in der Klasse Kunde an. Für die Eigenschaft name gibt es einen 
-#Lese- und Schreibzugriff, fuer die Eigenschaft adresse einen Schreibzugriff. 
-# b. Die Eigenschaft name enthält den Vor- und Zunamen des Kunden. Diese Eigenschaft wird beim Anlegen des Objektes übergeben. Schreiben Sie den entsprechenden Konstruktor. 
+# 1a. Legen Sie eine Eigenschaft name und adresse in der Klasse Kunde an. Für die Eigenschaft name
+# 	gibt es einen Lese- und Schreibzugriff, fuer die Eigenschaft adresse einen Schreibzugriff. 
+# b. Die Eigenschaft name enthält den Vor- und Zunamen des Kunden. Diese Eigenschaft wird
+# 		beim Anlegen des Objektes übergeben. Schreiben Sie den entsprechenden Konstruktor. 
 # c. Erzeugen Sie ein Objekt mit dem Namen 'kunde1' und dem Initialwert: 'Theo Sommer' 
 # d. Ändern Sie die Eigenschaft 'name' des Objektes in: 'Theo Sonnenschein' 
 # e. Schreiben Sie den Befehl, der den Namen auf der Konsole ausgibt. 
-# f. Fügen Sie der Klasse Kunde die Eigenschaft gehalt hinzu, die sowohl gelesen als auch geschrieben werden kann. Schreiben Sie den Befehl auf, der den Wert von 'gehalt' des Objektes auf 2000 setzt. 
+# f. Fügen Sie der Klasse Kunde die Eigenschaft gehalt hinzu, die sowohl gelesen als auch 
+# 	geschrieben werden kann. Schreiben Sie den Befehl auf, der den Wert von 'gehalt' des Objektes
+# auf 2000 setzt. 
 # g. Die Eigenschaft adresse bekommt folgenden String uebergeben: Hermelinweg 11, 22159 Hamburg
-# h. Schreiben Sie eine  Getter-Methode' für die Klasse Kunde, für die Eigenschaft adresse die folgende Ausgabe produziert: 'Theo Sonnenschein, Hermelinweg 11, 22159 Hamburg.'  und geben sie das auf der Konsole aus.
-# i. Zusatz optional: Sichern Sie, dass im String Theo Sonnenschein sowohl Theo, als auch Sonnenschein jeweils mit einem Grossbuchstaben beginnen
+# h. Schreiben Sie eine  Getter-Methode' für die Klasse Kunde, für die Eigenschaft adresse die
+# folgende Ausgabe produziert: 'Theo Sonnenschein, Hermelinweg 11, 22159 Hamburg.'  und geben
+# sie das auf der Konsole aus.
+# i. Zusatz optional: Sichern Sie, dass im String Theo Sonnenschein sowohl Theo, 
+# als auch Sonnenschein jeweils mit einem Grossbuchstaben beginnen
 
 
 class Kunde
@@ -18,18 +24,18 @@ class Kunde
 
 
 	attr_writer :adresse
-
+	attr_accessor :gehalt
 	attr_accessor :name
 
 
 
-def initialize(name, adresse)
+def initialize(name)
 		@name = name
-		@adresse = adresse
+		
 	end
 
-def to_s
-
+def huhu
+" Name:#{@name.split.first.capitalize} #{@name.split.last.capitalize}\n Adresse: #{@adresse} \n Gehalt: #{@gehalt}"
 end
 
 
@@ -37,6 +43,3 @@ end
 
 end
 
-kunde1= Kunde.new('Theo Sommer', 'Sonnenallee')
-puts "#{kunde1.name} wohnt in der #{kunde1.adresse}"
-puts kunde1.name
