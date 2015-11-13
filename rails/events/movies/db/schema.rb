c8b606ab735c66da7286dfef6874dc12bc3b5d8b
Klackamatsch <sarah.kaehlig@gmx.de>
2015-11-13 08:05:47 +0100

    freitag

--- rails/events/movies/db/schema.rb
+++ rails/events/movies/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112095844) do
+ActiveRecord::Schema.define(version: 20151112124406) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20151112095844) do
     t.decimal  "total_gross"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.datetime "kinostart"
+    t.text     "description"
   end
 
 end
