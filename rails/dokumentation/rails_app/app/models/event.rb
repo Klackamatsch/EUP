cf8a6b69f1d12325855977fab1a23151ababb453
Klackamatsch <sarah.kaehlig@gmx.de>
2015-11-19 14:45:37 +0100

    Donnerstag

--- rails/dokumentation/rails_app/app/models/event.rb
+++ rails/dokumentation/rails_app/app/models/event.rb
@@ -4,4 +4,9 @@ class Event < ActiveRecord::Base
 		self.price.blank? || self.price.zero?     # das kommt aus dem helper 
 		
 	end
+
+	def self.upcoming
+	where("start_at >= ? ", Time.now).order("start_at")
+		
+	end
 end
